3c47d146e311c519d22872d67a50b12da8caa6a1
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-11 14:44:42 +0100

    styling hamburger and mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-let bro;
+const navList = document.querySelector(".navbar__list");
+const navLinks = document.querySelectorAll(".navbar__list-item");
+const hamburgerBtn = document.querySelector(".hamburger");
+
+const toggleHamburger = () => {
+  hamburgerBtn.classList.toggle("is-active");
+  navList.classList.toggle("active");
+  document.body.classList.toggle("sticky-body");
+};
+
+const removeSticky = () => {
+  document.body.classList.remove("sticky-body");
+  navList.classList.remove("active");
+  hamburgerBtn.classList.remove("is-active");
+};
+
+hamburgerBtn.addEventListener("click", toggleHamburger);
+navLinks.forEach((links) => {
+  links.addEventListener("click", removeSticky);
+});
