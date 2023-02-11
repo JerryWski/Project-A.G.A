a5cb7990c0f06342e24629507808e20ba8c845a9
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-11 15:40:35 +0100

    animaton for links

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,30 @@
 const navList = document.querySelector(".navbar__list");
 const navLinks = document.querySelectorAll(".navbar__list-item");
+const linksSelect = document.querySelectorAll(".links");
 const hamburgerBtn = document.querySelector(".hamburger");
 
 const toggleHamburger = () => {
   hamburgerBtn.classList.toggle("is-active");
   navList.classList.toggle("active");
   document.body.classList.toggle("sticky-body");
+  handleNavLinksAnimate()
 };
 
 const removeSticky = () => {
   document.body.classList.remove("sticky-body");
   navList.classList.remove("active");
   hamburgerBtn.classList.remove("is-active");
+};
+
+const handleNavLinksAnimate = () => {
+  let delayTime = 0;
+
+  linksSelect.forEach((item) => {
+    item.classList.toggle("links--animation");
+
+    item.style.animationDelay = "." + delayTime + "s";
+    delayTime++;
+  });
 };
 
 hamburgerBtn.addEventListener("click", toggleHamburger);
