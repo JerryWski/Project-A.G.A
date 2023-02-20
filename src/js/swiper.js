b4abb42da287805b8d7587d81dd9cd2a3e8799a0
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-20 21:31:07 +0100

    finally working swiper, my tweak and mods

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,18 +1,46 @@
-const swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2,
-      slideShadows: true,
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    220: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    spaceBetween: 60,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-  });
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    950: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1250: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    }
+  },
+});
